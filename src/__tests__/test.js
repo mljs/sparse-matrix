8d42c1c3b6b5913b8e392ca1c3545bb439e59ed0
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-08 16:05:23 +0200

    feat: check if is a banded matrix

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -37,7 +37,33 @@ describe('Sparse Matrix', () => {
     });
 
     it('transpose', () => {
-        const matrix1 = new SparseMatrix([[1, 2], [3, 4]]);
-        expect(matrix1.transpose().to2DArray()).toEqual([[1, 3], [2, 4]]);
+        const matrix = new SparseMatrix([[1, 2], [3, 4]]);
+        expect(matrix.transpose().to2DArray()).toEqual([[1, 3], [2, 4]]);
+    });
+});
+
+describe('Banded matrices', () => {
+    it('Check band size', () => {
+        const matrix1 = new SparseMatrix([[1, 0], [0, 1]]);
+        const matrix2 = new SparseMatrix([[1, 0, 0], [0, 1, 0]]);
+        const matrix3 = new SparseMatrix([[1, 0, 1], [0, 1, 0]]);
+        const matrix4 = new SparseMatrix([[1, 0, 0], [1, 1, 0]]);
+        const matrix5 = new SparseMatrix([[0, 0, 0], [1, 0, 0], [0, 1, 0]]);
+        expect(matrix1.bandWidth()).toBe(0);
+        expect(matrix2.bandWidth()).toBe(0);
+        expect(matrix3.bandWidth()).toBe(2);
+        expect(matrix4.bandWidth()).toBe(1);
+        expect(matrix5.bandWidth()).toBe(0);
+    });
+
+    it('isBanded', () => {
+        const matrix1 = new SparseMatrix([[1, 0], [0, 1]]);
+        const matrix2 = new SparseMatrix([[1, 0, 0], [0, 1, 0]]);
+        const matrix3 = new SparseMatrix([[1, 0, 1], [0, 1, 0]]);
+        const matrix4 = new SparseMatrix([[1, 0, 0], [1, 1, 0]]);
+        expect(matrix1.isBanded(1)).toBe(true);
+        expect(matrix2.isBanded(1)).toBe(true);
+        expect(matrix3.isBanded(1)).toBe(false);
+        expect(matrix4.isBanded(1)).toBe(true);
     });
 });
