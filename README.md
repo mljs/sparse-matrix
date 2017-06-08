90b4cc6c9efe4b418ef97a243a1043473a8b8ea0
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-08 13:06:41 +0200

    chore: initial modification as modules

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # sparse-matrix
+
+  [![NPM version][npm-image]][npm-url]
+  [![build status][travis-image]][travis-url]
+  [![npm download][download-image]][download-url]
+
+Sparse matrix library.
+
+## Installation
+
+`$ npm install --save ml-sparse-matrix`
+
+## Usage
+
+```js
+import library from 'ml-sparse-matrix';
+
+const result = library(args);
+// result is ...
+```
+
+## [API Documentation](https://mljs.github.io/sparse-matrix/)
+
+## License
+
+  [MIT](./LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/ml-sparse-matrix.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/ml-sparse-matrix
+[travis-image]: https://img.shields.io/travis/mljs/sparse-matrix/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/mljs/sparse-matrix
+[download-image]: https://img.shields.io/npm/dm/ml-sparse-matrix.svg?style=flat-square
+[download-url]: https://npmjs.org/package/ml-sparse-matrix
---
+++ src/__tests__/test.js
@@ -0,0 +1,38 @@
+import SparseMatrix from '..';
+
+describe('Sparse Matrix', () => {
+    it('mmul', () => {
+        var m1 = new SparseMatrix([[2, 0, 1], [0, 0, 3]]);
+        var m2 = new SparseMatrix([[0, 1], [2, 0], [0, 0]]);
+        var m3 = m1.mmul(m2);
+
+        expect(m1.cardinality).toEqual(3);
+        expect(m2.cardinality).toEqual(2);
+        expect(m3.cardinality).toEqual(1);
+
+        expect(m3.get(0, 1)).toEqual(2);
+    });
+
+    it('kronecker', () => {
+        const matrix1 = new SparseMatrix([[1, 2], [3, 4]]);
+        const matrix2 = new SparseMatrix([[0, 5], [6, 7]]);
+        const product = matrix1.kroneckerProduct(matrix2);
+        expect(product.to2DArray()).toEqual([
+            [0, 5, 0, 10],
+            [6, 7, 12, 14],
+            [0, 15, 0, 20],
+            [18, 21, 24, 28]
+        ]);
+    });
+
+    it('isSymmetric', () => {
+        expect((new SparseMatrix(10, 10)).isSymmetric()).toEqual(true);
+        expect((new SparseMatrix(15, 10)).isSymmetric()).toEqual(false);
+
+        let m = new SparseMatrix([[0, 1], [1, 0]]);
+        expect(m.isSymmetric()).toEqual(true);
+
+        m = new SparseMatrix([[0, 1], [0, 1]]);
+        expect(m.isSymmetric()).toEqual(false);
+    });
+});
